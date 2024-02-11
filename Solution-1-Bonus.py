2650f5ede071356dbb640b04445ec6085ffbf477
Marwan <mrwan278@gmail.com>
2024-02-11 18:00:35 +0300

    made small changes

--- Solution-1-Bonus.py
+++ Solution-1-Bonus.py
@@ -1,6 +1,6 @@
 def Stairs(num_steps):
     print("Generating...")
-    for i in range(1, num_steps + 1):
+    for i in range(0, num_steps+1):
         print(' ' * (num_steps - i), end='')
         print('*' * i, end=' ')
         print(' ' * 2, end='')
--- Solution-1.py
+++ Solution-1.py
@@ -1,6 +1,6 @@
 def Stairs(num_steps):
     print("Generating...")
-    for i in range(0, num_steps + 1):
+    for i in range(0, num_steps):
         for j in range(0,i):
             print("*",end='')
         print("")

2a9aa262419f975bfa02ffe08061f747c6ee150f
Haya <145580738+H4ya@users.noreply.github.com>
2024-02-17 12:27:35 +0300

    Update task 1.1 Bonse.py

--- dev.unit-1.1/task 1.1 Bonse.py
+++ dev.unit-1.1/task 1.1 Bonse.py
@@ -1,10 +1,8 @@
-def staircase (x):
-    for i in range (1,x+1) :
-        for t in range (x-i):
-            print('  ',end='')
-        for j in range (i*2):
-            print ("*",end=' ')
-        print()
+def print_stair(x):
+    for i in range(1, x+1):
+        stars = '*'*i
+        space = (x-i) * " " 
+        print(space + stars + " " + stars)
 
-size= int ( input ( "Enter the wanted size: " ) )
-staircase(size)
+n=int(input("Enter size of the staircase: "))
+print_stair(n)

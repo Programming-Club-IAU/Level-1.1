c2104fea83ea6f9f32c78404c05b0ff911f88b15
Mariam5x <149164980+mana5x@users.noreply.github.com>
2024-02-17 11:54:43 +0300

    Update task1-Mariam-barakat.py

--- task1-Mariam.py
+++ task1-Mariam.py
@@ -1,6 +1,13 @@
 def staircase(S):
     for i in range(1, S + 1):
-        print('*'* i)
-
+        print('*' * i)
 S = int(input("Enter the size of the Staircase: "))
 staircase(S)
+
+print(" ----------------both sides of the staircase--------------\n")
+
+def both_side(both):
+ for i in range(1, both + 1):
+        print(' '*(both - i)+'*' * i +' ' +'*' * i)
+both = int(input("Enter the size of the Staircase: "))
+both_side(both)

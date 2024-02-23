ee31f35f12aff7011a1bbbef9e365109344f6573
Mahabarakat <160283857+Mahabarakat@users.noreply.github.com>
2024-02-23 22:35:52 +0300

    Add files via upload

---
+++ task2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class task2 {
+    public static void main(String[] args){
+        Scanner enter=new Scanner(System.in);
+        System.out.println("Enter the first number");
+        int first= enter.nextInt();
+        System.out.println("Enter the second number");
+        int second= enter.nextInt();
+        if(first>second)
+            System.out.println(first+" is grater than "+second );
+        
+        else if(second>first)
+            System.out.println(second+" is grater than "+first );
+
+        
+        else
+            System.out.println("The two numbers are equal");
+
+        
+    }
+}
+
